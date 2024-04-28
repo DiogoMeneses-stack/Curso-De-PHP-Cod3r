e68cb26637cc39e2b14141cba985e4a4cf860cf6
Diogo Meneses(真龍)☸️ <henriquediogo434@gmail.com>
2024-04-28 20:30:41 -0300

    Funções e Escopo

--- funcoes/escopo.php
+++ funcoes/escopo.php
@@ -3,5 +3,21 @@
 <?php
 
 function imprimirMensagem(){
-    
+    echo "Olá";
+    echo "Olá até a próxima <br>";
 }
+
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
+
+$variavel = 1;
+
+function trocarValor(){
+    $variavel = 2;
+    echo "Durante a função $variavel <br>";
+}
+
+echo "Antes da Função $variavel <br>";
+trocarValor();
+echo "Depois $variavel <br>";
