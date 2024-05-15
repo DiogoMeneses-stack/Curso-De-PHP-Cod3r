6b5bc8ca6f73bbc960b512ceef136f729da5ed86
= <henriquediogo434@gmail.com>
2024-05-15 17:32:55 -0300

    Herança início.

--- POO/herança.php
+++ POO/herança.php
@@ -2,3 +2,21 @@
 
 <?php
 
+class Pessoa{
+    public $nome;
+    public $idade;
+
+    function __construct($novoNome, $idade){
+        $this->nome = $novoNome;
+        $this->idade = $idade;
+        echo 'Pessoa Criada! <br>';
+    }
+
+    function __destruct(){
+        echo 'falou!!!';
+    }
+
+    public function apresentar(){
+        echo "{$this->nome}, {$this->idade} anos<br>";
+    }
+}
