e6f5ed39c920ecbcd8328054281c375bc427c4cc
= <henriquediogo434@gmail.com>
2024-05-24 16:52:18 -0300

    modificador final e traits

--- POO/traits.php
+++ POO/traits.php
@@ -1,7 +1,7 @@
 <div class="titulo">Traits</div>
 
 <?php
-
+echo "=============Traits 1============= <br>";
 trait validacao{
     public $a = 'Válido';
     public function validarString($str){
@@ -25,4 +25,33 @@ var_dump($usuario->validarString(''));
 echo '<br>';
 var_dump($usuario->validarStringMelhor(''));
 echo '<br>';
-echo $usuario->a, '<br>' , $usuario->b;
+echo $usuario->a, '<br>' , $usuario->b . "<br>";
+
+echo "=============Traits 2=============<br>";
+
+trait valido{
+    public function validarString($str){
+        return isset($str)  && $str !== '';
+    }
+}
+
+trait validarBem{
+    public function validarString($str){
+        return isset($str) && trim($str);
+    }
+}
+
+class Usuario2{
+    use valido, validarBem{
+        // resolução de conflitos com traits
+        validarBem::validarString insteadOf valido; // a 1
+
+        valido::validarString as validacaoSimples; // a 2
+
+    }
+}
+
+$usuario = new Usuario2();
+var_dump($usuario->validarString(''));
+echo '<br>';
+var_dump($usuario->validacaoSimples(''));
