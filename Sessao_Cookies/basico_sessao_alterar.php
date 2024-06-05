45bfba75720d77dd37f1309af2cf4d1d64616465
= <henriquediogo434@gmail.com>
2024-06-05 15:04:34 -0300

    Sessao 02 e 03

---
+++ Sessao_Cookies/basico_sessao_alterar.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    print_r($_SESSION);
+    ?>
+
+    <p>
+        <b>Nome:</b> <?= $_SESSION['nome'] ?>
+    </p>
+    <p>
+        <b>Email:</b> <?= $_SESSION['email'] ?>
+    </p>
+
+    <?php
+    $_SESSION['email'] = 'diogofilho@gmail.com';
+    ?>
+
+    <p>
+        <a href="sessao_01.php">Voltar</a>
+    </p>
+
+    <p>
+        <a href="basico_sessao_limpar.php">Limpar Sessão</a>
+    </p>
