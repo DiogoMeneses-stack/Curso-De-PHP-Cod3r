59d79c26312342847ea7e8c82c22c62956c6ed4b
= <henriquediogo434@gmail.com>
2024-06-14 14:11:32 -0300

    Fomulário 02

--- formulario/criando-form.php
+++ formulario/criando-form.php
@@ -2,7 +2,50 @@
 <div class="titulo">Criando Formulário</div>
 <h2>Cadastro</h2>
 
-<?php 
+<?php
+//Validações de Formulário
+if (count($_POST) > 0) {
+    
+    //isset($_POST['nome'])
+    
+//Validação do Nome
+    if (!filter_input(INPUT_POST, "nome")) {
+        echo 'Nome é obrigatório', '<br>';
+    }
+
+// Validação de Data
+    if (filter_input(INPUT_POST, "nascimento")) {
+        $data = DateTime::createFromFormat('d/m/Y', $_POST['nascimento']);
+        if (!$data) {
+            echo 'Data dev estar no padrão dd/mm/aaaa', '<br>';
+        }
+    }
+
+    //Validação Email
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo 'Email inválido', '<br>';
+    }
+
+    // Validação site
+    if (!filter_var($_POST['site'], FILTER_VALIDATE_URL)) {
+        echo 'Site inválido', '<br>';
+    }
+
+// Validação de Filho
+    $filhosConfig = [
+        "options" => ["min_renge"=> 0, "max_range"=> 20]
+    ];
+    if(!filter_var($_POST['filhos'], FILTER_VALIDATE_INT, 
+    $filhosConfig) && $_POST['filhos'] != 0){
+        echo 'Quantidade de filhos inválida', '<br>';
+    }
+
+// Validação de Salário
+    $salarioConfig = ['options' => ['decimal' => ',']];
+    if(!filter_var($_POST['salario'], FILTER_VALIDATE_FLOAT, $salarioConfig)){
+        echo 'Salário inválido', '<br>';
+    }
+}
 
 ?>
 
@@ -10,40 +53,33 @@
     <div class="form-row">
         <div class="form-group col-md-8">
             <label for="nome">Nome</label>
-            <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" 
-            value="<?= $_POST['nome']?>"/>
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Nome" value="<?= $_POST['nome'] ?>" />
         </div>
         <div class="form-group col-md-4">
             <label for="nascimento">Nascimento</label>
-            <input type="text" class="form-control" id="nascimento" name="nascimento" placeholder="Nascimento" 
-            value="<?= $_POST['nascimento']?>"/>
+            <input type="text" class="form-control" id="nascimento" name="nascimento" placeholder="Nascimento" value="<?= $_POST['nascimento'] ?>" />
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="email">Email</label>
-            <input type="text" class="form-control" id="email" name="email" placeholder="E-mail" 
-            value="<?= $_POST['email']?>"/>
+            <input type="text" class="form-control" id="email" name="email" placeholder="E-mail" value="<?= $_POST['email'] ?>" />
         </div>
 
         <div class="form-group col-md-6">
             <label for="site">Site</label>
-            <input type="text" class="form-control" id="site" name="site" placeholder="Site" 
-            value="<?= $_POST['site']?>"/>
+            <input type="text" class="form-control" id="site" name="site" placeholder="Site" value="<?= $_POST['site'] ?>" />
         </div>
     </div>
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="filhos">Qtde de Filhos</label>
-            <input type="text" class="form-control" id="filhos" name="filhos" placeholder="Filhos" 
-            value="<?= $_POST['filhos']?>"/>
+            <input type="number" class="form-control" id="filhos" name="filhos" placeholder="Filhos" value="<?= $_POST['filhos'] ?>" />
         </div>
         <div class="form-group col-md-6">
             <label for="salario">Salário</label>
-            <input type="text" class="form-control" id="salario" name="salario" placeholder="Salario" 
-            value="<?= $_POST['salario']?>"/>
+            <input type="text" class="form-control" id="salario" name="salario" placeholder="Salario" value="<?= $_POST['salario'] ?>" />
         </div>
     </div>
     <button class="btn btn-primary btn-lg">Enviar</button>
 </form>
-
